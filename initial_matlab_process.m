e844eac9fb2be4cc5c7b741eded32a0123e42337
Aaron Marburg <aaron@uw.edu>
2019-10-24 15:31:59 -0700

    Initial experiments getting top-level script running.

--- initial_matlab_process.m
+++ initial_matlab_process.m
@@ -5,3 +5,4 @@ addpath(genpath('master_program'))
 addpath(genpath('ThirdParty/GSW/'))
 
  covis = covis_raw_sweep('covis-test-data/','COVIS-20191024T003002-diffuse1.tar.gz',0)
+ save('output.mat', 'covis')
