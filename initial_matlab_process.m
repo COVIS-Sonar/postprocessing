2210d29b89b8b602878f0e5988acb9e6df029809
Aaron Marburg <aaron@coho.san.apl.washington.edu>
2019-10-24 14:40:14 -0700

    Remove Common/
    Also add "make dep" rule to Makefile.

---
+++ initial_matlab_process.m
@@ -0,0 +1,7 @@
+
+%% Matlab -nodisplay
+
+addpath(genpath('master_program'))
+addpath(genpath('ThirdParty/GSW/'))
+
+ covis = covis_raw_sweep('covis-test-data/','COVIS-20191024T003002-diffuse1.tar.gz',0)
