cbba5fc681bb3909365a096664f7f375571aea2f
Aaron Marburg <aaron@uw.edu>
2019-11-04 13:34:43 -0800

    Successful processing in Docker image.

---
+++ Deploy/matlab/process.m
@@ -0,0 +1,14 @@
+function [output] = process( inputFile, outputFile )
+
+%
+% Some portion of the output structure cannot be passed back to Python
+% space.  Instead, do some of the processing in Matlab space and return
+% a "python-sanitized" data structure.
+%
+
+[filepath,name,ext] = fileparts( inputFile );
+
+covis = covis_raw_sweep(filepath,strcat(name,ext),0)
+save(outputFile, 'covis')
+
+output = {}
--- Deploy/pycovis-postprocess/scripts/process.py
+++ Deploy/pycovis-postprocess/scripts/process.py
@@ -6,12 +6,14 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Process one COVIS file.')
 parser.add_argument('inputFile', help='COVIS file to parse')
-parser.add_argument("--output",  help="Filename for output", default="")
+parser.add_argument("--output",  help="Filename for output",
+                        dest="outputMat", default="")
 
 args = parser.parse_args()
 
 
 inputPath = Path(args.inputFile).resolve()
+outputPath = Path(args.outputMat).resolve()
 
 if not inputPath.exists():
     sys.exit( "Input file %s doesn't exist" % inputPath )
@@ -20,11 +22,11 @@ print("Processing input file: %s" % inputPath )
 
 with runtime.Runtime() as covis:
 
-    result = covis.covis_raw_sweep( str(inputPath.parent), str(inputPath.name), 0 )
+    result = covis.process( str(inputPath), str(outputPath) )
 
-    if result:
-        print("Saving results to %s" % result)
-        covis.save( args.output, result )
+    # if result:
+    #     print("Saving results to %s" % result)
+    #     covis.save( args.output, result )
 
     # matfile = pp.covis_imaging_sweep(input, outdir, '')
     # imgfile = pp.covis_imaging_plot(matfile, outdir, '')
