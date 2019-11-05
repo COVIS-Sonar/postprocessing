9a318ff866babaa4c682e5ebd30e7f58347f0f64
Aaron Marburg <amarburg@uw.edu>
2019-11-04 16:38:01 -0800

    Add tools for evaluating imaging data.

--- initial_matlab_process.m
+++ initial_matlab_process.m
@@ -4,5 +4,7 @@
 addpath(genpath('master_program'))
 addpath(genpath('ThirdParty/GSW/'))
 
- covis = covis_raw_sweep('TestData/','COVIS-20191024T003346-diffuse3.tar.gz',0)
- save('output.mat', 'covis')
+%covis = covis_raw_sweep('TestData/','COVIS-20191024T003346-diffuse3.tar.gz',0)
+
+covis = covis_raw_sweep('TestData/','COVIS-20191024T000002-imaging1.tar.gz',0)
+save('COVIS-20191024T000002-imaging1.mat', 'covis')
