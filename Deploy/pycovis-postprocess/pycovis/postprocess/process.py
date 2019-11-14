2cbc6db7dff6e05f455d0a634d9e64a67211c4da
Aaron Marburg <amarburg@uw.edu>
2019-11-13 21:26:14 -0800

    Migrating s3 upload/download to covis-worker.

--- Deploy/pycovis-postprocess/pycovis/postprocess/process.py
+++ Deploy/pycovis-postprocess/pycovis/postprocess/process.py
@@ -19,93 +19,11 @@ from pyunpack import Archive
 
 def process( inputFile, outputDir ):
 
-#parser = argparse.ArgumentParser(description='Process one COVIS file.')
-
-# parser.add_argument('inputFile', help='COVIS file to parse')
-# parser.add_argument("--output",  help="Directory for output",
-#                         dest="outputDir", default="/output")
-#
-# parser.add_argument("--auto-output-path", action='store_true',
-#                     help="Automatically add the YYYY/MM/DD/ path to the output")
-#
-# parser.add_argument('--input-s3-host', default=config('INPUT_S3_HOST', default=""),
-#                     dest="inputS3Host",
-#                     help='Hostname for input S3 server (if s3:// URL is provided for inputFile)')
-# parser.add_argument('--input-s3-access-key', default=config('INPUT_S3_ACCESS_KEY', default=""),
-#                     dest="inputS3AccessKey",
-#                     help='Access key for input S3 server (if s3:// URL is provided for inputFile)')
-# parser.add_argument('--input-s3-secret-key', default=config('INPUT_S3_SECRET_KEY', default=""),
-#                     dest="inputS3SecretKey",
-#                     help='Secret key for input S3 server (if s3:// URL is provided for inputFile)')
-#
-# parser.add_argument('--output-s3-host', default=config('OUTPUT_S3_HOST', default=""),
-#                     dest="outputS3Host",
-#                     help='Hostname for output S3 server (if s3:// URL is provided for outputFile)')
-# parser.add_argument('--output-s3-access-key', default=config('OUTPUT_S3_ACCESS_KEY', default=""),
-#                     dest="outputS3AccessKey",
-#                     help='Access key for output S3 server (if s3:// URL is provided for outputFile)')
-# parser.add_argument('--output-s3-secret-key', default=config('OUTPUT_S3_SECRET_KEY', default=""),
-#                     dest="outputS3SecretKey",
-#                     help='Secret key for output S3 server (if s3:// URL is provided for outputFile)')
-#
-# args = parser.parse_args()
-
-
     ## Parse the input file,
-    input = urlparse( inputFile )
-    output = urlparse( outputDir )
-
-    outputMinioClient = None
-
-    ## Validate the output first, rather than catch it at the end
-    if output.scheme == '':
-        ## If writing to file, work directly in the final destination
-        workingOutputPath = Path(output.path)
-
-    elif output.scheme == 's3':
-        s3host = config('OUTPUT_S3_HOST', default="")
-        if not s3host:
-            sys.exit("s3:// output URL provided but --output-s3-host not provided")
-
-        outputMinioClient = Minio(s3host,
-                      access_key=config('OUTPUT_S3_ACCESS_KEY', default=""),
-                      secret_key=config('OUTPUT_S3_SECRET_KEY', default=""),
-                      secure=False)
-
-        ## Otherwise, set a default working directory
-        workingOutputPath = Path("/output")
-
-        ## ...
-
+    inputPath = Path( inputFile ).resolve()
+    outputPath = Path( outputDir ).resolve()
 
     tempdir = tempfile.TemporaryDirectory(prefix = 'covis')
-
-    ## Now deal with the input
-    ## If a local file
-    if input.scheme == '':
-        inputPath = Path(input.path).resolve()
-    elif input.scheme == 's3':
-
-        s3host = config('INPUT_S3_HOST', default="")
-        if not s3host:
-            sys.exit("s3:// input URL provided but --input-s3-host not provided")
-
-        bucket = input.netloc
-        path   = input.path
-
-        print("Retrieving bucket: %s, path %s from host %s" % (bucket, path, s3host))
-
-        minioClient = Minio(s3host,
-                      access_key=config('INPUT_S3_ACCESS_KEY', default=""),
-                      secret_key=config('INPUT_S3_SECRET_KEY', default=""),
-                      secure=False)
-
-        basename = Path(path).name
-        inputPath = Path( tempdir.name ) / basename
-
-        print("Retrieving path %s from bucket %s to %s" % (path, bucket, inputPath))
-
-        minioClient.fget_object(bucket_name=bucket, object_name=path, file_path=str(inputPath) )
 
     if not inputPath.exists():
         sys.exit( "Input file %s doesn't exist" % inputPath )
@@ -114,7 +32,7 @@ def process( inputFile, outputDir ):
     basename = re.sub(r'\.tar','',basename)   ## Pathlib::stem only removes the .gz from .tar.gz
 
     matOutput = basename + ".mat"
-    matOutputPath = workingOutputPath / matOutput
+    matOutputPath = outputPath / matOutput
 
     print("Processing input file: %s" % inputPath )
     print("            to output: %s" % matOutputPath )
@@ -128,7 +46,7 @@ def process( inputFile, outputDir ):
 
         ## \TODO  Need a way to find the directory name found in the archive rather
         ## than this open-loop version
-        unpackedInput = Path(tempdir.name) / basename
+        unpackedInput = Dir.glob( tempdir.name + "/*" )[0]  / basename
 
     ## Tough to diagnose what's happening with the unpacked archive
     # for x in os.listdir( tempdir.name ):
@@ -140,26 +58,3 @@ def process( inputFile, outputDir ):
 
     with runtime.Runtime() as covis:
         result = covis.process( str(unpackedInput), str(matOutputPath) )
-
-    ## Reuse from above
-    if outputMinioClient:
-
-        outbucket = output.netloc
-
-        if not outputMinioClient.bucket_exists( outbucket ):
-            outputMinioClient.make_bucket(outbucket)
-
-        ## \TODO  generate correct outputName
-        outpath = Path(output.path) / matOutput
-        outpath = str(outpath).strip("/")  ## Minio client doesn't like leading slash
-
-        outputMinioClient.fput_object(file_path=matOutputPath, bucket_name=outbucket, object_name=outpath)
-
-        print("Uploaded to bucket %s, path %s" % (outbucket,outpath) )
-
-        # if result:
-        #     print("Saving results to %s" % result)
-        #     covis.save( args.output, result )
-
-        # matfile = pp.covis_imaging_sweep(input, outdir, '')
-        # imgfile = pp.covis_imaging_plot(matfile, outdir, '')
