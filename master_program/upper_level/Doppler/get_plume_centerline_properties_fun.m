adb448bad033c3cb659e13e747b39755fe0f6383
Guangyu Xu <37380561+xupeng66@users.noreply.github.com>
2020-07-08 15:45:42 -0700

    preliminary commit

--- master_program/upper_level/Doppler/get_plume_centerline_properties_fun.m
+++ master_program/upper_level/Doppler/get_plume_centerline_properties_fun.m
@@ -4,7 +4,7 @@
 % on each cross-section to calculate those properties for the plume above
 % Inferno.
 
-% function center = get_plume_centerline_properties_fun(grd,xs,ys,Ri,Ro,diagnosis)
+function center = get_plume_centerline_properties_fun(grd,xs,ys,Ri,Ro,zmin,zmax,diagnosis)
 % Input:
 % grd: gridded data
 % xs: x-coordinate of sourve vent (e.g., Inferno) (m)
@@ -34,8 +34,6 @@
 % set up grid parameters
 xrange = [xs-Ro,xs+Ro];
 yrange = [ys-Ro,ys+Ro];
-zmin = 2.5;
-zmax = 14;
 xg=grd.x;
 yg=grd.y;
 zg=grd.z;
@@ -209,7 +207,7 @@ center.SSDev = SSDev_out;
 center.SST = SST_out;
 center.Npts = Npts_out;
 center.Flag = Flag_out;
-%end
+end
 
 
 
